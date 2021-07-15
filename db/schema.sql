3b435cd83fd7ee2bc7f864ba8ec5ad430a7cce54
Tyler Hance <tyler.hance@gmail.com>
2021-07-15 16:12:32 -0700

    Added seed data for testing.

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,20 @@ USE employee_trackerDB;
 -- Department DB -- 
 CREATE TABLE department (
     id INT PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
     department_id INT
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT
 );
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department(department_name)
+VALUES ("Marketing"), ("Sales"), ("Finance"), ("HR"), ("Engineering");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Marketing Director", 90000, 1), ("Marketing Assistant", 65000, 1), ("Senior Engineer", 130000, 5), ("Engineer", 80000, 5), ("CFO", 275000, 3), ("Sales Manager", 75000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Smith", 1, null), ("Greg", "Johnson", 1, 1), ("Jim", "White", 3, null), ("Bob", "Long", 5, 5), ("Frank", "Jones", 5, null);
