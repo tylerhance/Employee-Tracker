4bd445ba8a0887d4fd370aaf3d9701bed71336ab
Tyler Hance <tyler.hance@gmail.com>
2021-07-15 16:46:11 -0700

    Added inquirer prompts for user choices in server.js

--- server.js
+++ server.js
@@ -18,3 +18,75 @@ connection.connect((error) => {
     promptUser();
 });
 
+// User/Employer prompts
+const promptUser = () => {
+    inquirer.prompt([
+        {
+            name: "choices",
+            type: "list",
+            message: "Please select an option: ",
+            choices: [
+                "View all employees",
+                "View all roles",
+                "view all departments",
+                "View all employees by department",
+                "View department budgets",
+                "Update employee role",
+                "Update employee manager",
+                "Add employee",
+                "Add role",
+                "Add department",
+                "Remove employee",
+                "Remove role",
+                "Remove department",
+                "Exit"
+            ]
+        }
+    ])
+    .then((answers) => {
+        const {choices} = answers;
+
+        if (choices === "View all employees") {
+            viewAllEmployees();
+        }
+        if (choices === "View all roles") {
+            viewAllRoles();
+        }
+        if (choices === "View all departments") {
+            viewAllDepartments();
+        }
+        if (choices === "View all employees by department") {
+            viewEmployeesByDepartment();
+        }
+        if (choices === "View department budgets") {
+            viewDepartmentBudget();
+        }
+        if (choices === "Update employee role") {
+            updateEmployeeRole();
+        }
+        if (choices === "Update employee manager") {
+            updateEmployeeManager();
+        }
+        if (choices === "Add employee") {
+            addEmployee();
+        }
+        if (choices === "Add role") {
+            addRole();
+        }
+        if (choices === "Add department") {
+            addDepartment();
+        }
+        if (choices === "Remove employee") {
+            removeEmployee();
+        }
+        if (choices === "Remove role") {
+            removeRole();
+        }
+        if (choices === "Remove department") {
+            removeDepartment();
+        }
+        if (choices === "Exit") {
+            connection.end();
+        }
+    });
+};
