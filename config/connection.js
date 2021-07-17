935ce2c72a35c3274871ebf0a496b44523754730
Tyler Hance <tyler.hance@gmail.com>
2021-07-17 15:38:47 -0700

    Fixing server.js errors

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 require("dotenv").config();
 
-const connection = mysql.creatConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
